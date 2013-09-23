fb911a13200cd4aa52a43da0dad0366e77779628
rinconj <rinconj@news.com.au>
2013-09-23 10:36:36 +1000

    add bash custom

--- bash-setup.sh
+++ bash-setup.sh
@@ -13,6 +13,6 @@ cd $HOME
 ln -sb dotfiles/.screenrc .
 # ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
-# ln -sb dotfiles/.bashrc_custom .
+ln -sb dotfiles/.bashrc_custom .
 # ln -sf dotfiles/.emacs.d .
 
