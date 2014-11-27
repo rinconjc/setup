e6cf6edf30f7cc728e00e3c50721a1e1ece04ac3
Julio Rincon <julio.r@github.com>
2014-11-28 10:11:06 +1100

    remove du alias

--- setup.sh
+++ setup.sh
@@ -4,28 +4,30 @@
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
-sudo apt-get install -y git-core
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+# sudo apt-get install -y git-core
+# curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# source $HOME/.nvm/nvm.sh
+# nvm install v0.10.12
+# nvm use v0.10.12
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
-npm install -g jshint
+# npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
+# sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+#sudo apt-add-repository -y ppa:cassou/emacs
+#sudo apt-get update
+#sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
+sudo apt-get install -y emacs24
+sudo apt-get install -y tmux
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
@@ -39,7 +41,7 @@ if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
 git clone https://github.com/julior/dotfiles.git
-ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.tmux.conf .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
